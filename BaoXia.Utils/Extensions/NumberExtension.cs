1fbaf828b06c4ef93aa9963ab7e44472377419ef
ZhuXiaoChun <zhu.xiaochun@qq.com>
2023-10-31 19:01:46 +0800

    发布了： 7.0.8 版本。

--- BaoXia.Utils/Extensions/NumberExtension.cs
+++ BaoXia.Utils/Extensions/NumberExtension.cs
@@ -53,6 +53,123 @@ namespace BaoXia.Utils.Extensions
                         return number.Value;
                 }
 
+                public static int IfNullOrLessEqualThanReturn(
+                        this int? number,
+                        int defaultValue,
+                        int compareValue = 0)
+                {
+                        if (number == null
+                                || number <= compareValue)
+                        {
+                                return defaultValue;
+                        }
+                        return number.Value;
+                }
+
+                public static float IfNullOrLessEqualThanReturn(
+                        this float? number,
+                        float defaultValue,
+                        float compareValue = 0.0F)
+                {
+                        if (number == null
+                                || number <= compareValue)
+                        {
+                                return defaultValue;
+                        }
+                        return number.Value;
+                }
+
+                public static double IfNullOrLessEqualThanReturn(
+                        this double? number,
+                        double defaultValue,
+                        double compareValue = 0.0)
+                {
+                        if (number == null
+                                || number <= compareValue)
+                        {
+                                return defaultValue;
+                        }
+                        return number.Value;
+                }
+
+                public static int IfNullOrGreaterThanReturn(
+                        this int? number,
+                        int defaultValue,
+                        int compareValue = 0)
+                {
+                        if (number == null
+                                || number > compareValue)
+                        {
+                                return defaultValue;
+                        }
+                        return number.Value;
+                }
+
+                public static float IfNullOrGreaterThanReturn(
+                        this float? number,
+                        float defaultValue,
+                        float compareValue = 0.0F)
+                {
+                        if (number == null
+                                || number > compareValue)
+                        {
+                                return defaultValue;
+                        }
+                        return number.Value;
+                }
+
+                public static double IfNullOrGreaterThanReturn(
+                        this double? number,
+                        double defaultValue,
+                        double compareValue = 0.0)
+                {
+                        if (number == null
+                                || number > compareValue)
+                        {
+                                return defaultValue;
+                        }
+                        return number.Value;
+                }
+
+                public static int IfNullOrGreaterEqualThanReturn(
+                        this int? number,
+                        int defaultValue,
+                        int compareValue = 0)
+                {
+                        if (number == null
+                                || number >= compareValue)
+                        {
+                                return defaultValue;
+                        }
+                        return number.Value;
+                }
+
+                public static float IfNullOrGreaterEqualThanReturn(
+                        this float? number,
+                        float defaultValue,
+                        float compareValue = 0.0F)
+                {
+                        if (number == null
+                                || number >= compareValue)
+                        {
+                                return defaultValue;
+                        }
+                        return number.Value;
+                }
+
+                public static double IfNullOrGreaterEqualThanReturn(
+                        this double? number,
+                        double defaultValue,
+                        double compareValue = 0.0)
+                {
+                        if (number == null
+                                || number >= compareValue)
+                        {
+                                return defaultValue;
+                        }
+                        return number.Value;
+                }
+
                 #endregion
         }
 }

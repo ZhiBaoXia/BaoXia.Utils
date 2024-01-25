361fbee7659adb0009266e8d47fe13bb8824fc48
ZhuXiaoChun <zhu.xiaochun@qq.com>
2024-01-25 21:54:07 +0800

    完成了： 7.0.19 版本的功能收集、开发。

--- BaoXia.Utils/Extensions/UdpClientExtension.cs
+++ BaoXia.Utils/Extensions/UdpClientExtension.cs
@@ -2,114 +2,113 @@
 using System.Net;
 using System.Net.Sockets;
 
-namespace BaoXia.Utils.Extensions
+namespace BaoXia.Utils.Extensions;
+
+public static class UdpClientExtension
 {
-	public static class UdpClientExtension
+
+	////////////////////////////////////////////////
+	// @静态常量
+	////////////////////////////////////////////////
+
+	#region 静态常量
+
+	public class UdpClientListenInfo
 	{
+		public UdpClient UdpClient { get; set; }
 
-		////////////////////////////////////////////////
-		// @静态常量
-		////////////////////////////////////////////////
+		public IPEndPoint ListenIPEndPoint { get; set; }
 
-		#region 静态常量
+		public AsyncCallback ToReceiveMessage { get; set; }
 
-		public class UdpClientListenInfo
-		{
-			public UdpClient UdpClient { get; set; }
+		public Func<UdpClientListenInfo?, bool>? ToWillContinueReceiveBytes { get; set; }
 
-			public IPEndPoint ListenIPEndPoint { get; set; }
-
-			public AsyncCallback ToReceiveMessage { get; set; }
-
-			public Func<UdpClientListenInfo?, bool>? ToWillContinueReceiveBytes { get; set; }
-
-			public UdpClientListenInfo(
-				UdpClient udpClient,
-				IPEndPoint listenIPEndPoint,
-				Func<UdpClient, IPEndPoint?, byte[]?, Exception?, bool> toReceiveMessage,
-				Func<UdpClientListenInfo?, bool>? toWillContinueReceiveBytes)
-			{
-				UdpClient = udpClient;
-				ListenIPEndPoint = listenIPEndPoint;
-
-				ToReceiveMessage = new AsyncCallback((IAsyncResult receiveResult) =>
-				{
-					if (receiveResult.AsyncState is not UdpClientListenInfo udpClientListenInfo)
-					{
-						return;
-					}
-
-					IPEndPoint? remoteEndPoint = null;
-					byte[]? bytesReceived = null;
-					Exception? exceptionOfReceiveBytes = null;
-					try
-					{
-						remoteEndPoint = udpClientListenInfo.ListenIPEndPoint;
-						bytesReceived = udpClientListenInfo.UdpClient.EndReceive(
-							receiveResult,
-							ref remoteEndPoint);
-					}
-					catch (Exception exception)
-					{
-						exceptionOfReceiveBytes = exception;
-					}
-
-					////////////////////////////////////////////////
-					// !!!
-					var receiveMessageResult = toReceiveMessage(
-						udpClientListenInfo.UdpClient,
-						remoteEndPoint,
-						bytesReceived,
-						exceptionOfReceiveBytes);
-					// !!!
-					////////////////////////////////////////////////
-
-					if (!receiveMessageResult)
-					{
-						return;
-					}
-					var toWillContinueReceiveBytes = udpClientListenInfo.ToWillContinueReceiveBytes;
-					if (toWillContinueReceiveBytes == null
-					|| toWillContinueReceiveBytes(udpClientListenInfo))
-					{
-						udpClientListenInfo.UdpClient.BeginReceive(
-							udpClientListenInfo.ToReceiveMessage,
-							udpClientListenInfo);
-					}
-				});
-				ToWillContinueReceiveBytes = toWillContinueReceiveBytes;
-			}
-		}
-
-		#endregion
-
-
-		////////////////////////////////////////////////
-		// @类方法
-		////////////////////////////////////////////////
-
-		#region 类方法
-
-		public static IAsyncResult StartReceiveBytesUtil(
-			this UdpClient udpClient,
-			Func<UdpClientListenInfo?, bool>? toWillContinueReceiveBytes,
+		public UdpClientListenInfo(
+			UdpClient udpClient,
 			IPEndPoint listenIPEndPoint,
-			Func<UdpClient, IPEndPoint?, byte[]?, Exception?, bool> toReceiveMessage)
+			Func<UdpClient, IPEndPoint?, byte[]?, Exception?, bool> toReceiveMessage,
+			Func<UdpClientListenInfo?, bool>? toWillContinueReceiveBytes)
 		{
-			var udpClientListenInfo = new UdpClientListenInfo(
-				udpClient,
-				listenIPEndPoint,
-				toReceiveMessage,
-				toWillContinueReceiveBytes);
+			UdpClient = udpClient;
+			ListenIPEndPoint = listenIPEndPoint;
 
-			var asyncResult = udpClient.BeginReceive(
-				udpClientListenInfo.ToReceiveMessage,
-				udpClientListenInfo);
+			ToReceiveMessage = new AsyncCallback((IAsyncResult receiveResult) =>
 			{
-			}
-			return asyncResult;
-		}
+				if (receiveResult.AsyncState is not UdpClientListenInfo udpClientListenInfo)
+				{
+					return;
+				}
 
-		#endregion
+				IPEndPoint? remoteEndPoint = null;
+				byte[]? bytesReceived = null;
+				Exception? exceptionOfReceiveBytes = null;
+				try
+				{
+					remoteEndPoint = udpClientListenInfo.ListenIPEndPoint;
+					bytesReceived = udpClientListenInfo.UdpClient.EndReceive(
+						receiveResult,
+						ref remoteEndPoint);
+				}
+				catch (Exception exception)
+				{
+					exceptionOfReceiveBytes = exception;
+				}
+
+				////////////////////////////////////////////////
+				// !!!
+				var receiveMessageResult = toReceiveMessage(
+					udpClientListenInfo.UdpClient,
+					remoteEndPoint,
+					bytesReceived,
+					exceptionOfReceiveBytes);
+				// !!!
+				////////////////////////////////////////////////
+
+				if (!receiveMessageResult)
+				{
+					return;
+				}
+				var toWillContinueReceiveBytes = udpClientListenInfo.ToWillContinueReceiveBytes;
+				if (toWillContinueReceiveBytes == null
+				|| toWillContinueReceiveBytes(udpClientListenInfo))
+				{
+					udpClientListenInfo.UdpClient.BeginReceive(
+						udpClientListenInfo.ToReceiveMessage,
+						udpClientListenInfo);
+				}
+			});
+			ToWillContinueReceiveBytes = toWillContinueReceiveBytes;
+		}
 	}
+
+	#endregion
+
+
+	////////////////////////////////////////////////
+	// @类方法
+	////////////////////////////////////////////////
+
+	#region 类方法
+
+	public static IAsyncResult StartReceiveBytesUtil(
+		this UdpClient udpClient,
+		Func<UdpClientListenInfo?, bool>? toWillContinueReceiveBytes,
+		IPEndPoint listenIPEndPoint,
+		Func<UdpClient, IPEndPoint?, byte[]?, Exception?, bool> toReceiveMessage)
+	{
+		var udpClientListenInfo = new UdpClientListenInfo(
+			udpClient,
+			listenIPEndPoint,
+			toReceiveMessage,
+			toWillContinueReceiveBytes);
+
+		var asyncResult = udpClient.BeginReceive(
+			udpClientListenInfo.ToReceiveMessage,
+			udpClientListenInfo);
+		{
+		}
+		return asyncResult;
+	}
+
+	#endregion
 }
--- BaoXia.Utils/Extensions/WebSocketExtension.cs
+++ BaoXia.Utils/Extensions/WebSocketExtension.cs
@@ -7,286 +7,285 @@ using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace BaoXia.Utils.Extensions
+namespace BaoXia.Utils.Extensions;
+
+public static class WebSocketExtension
 {
-	public static class WebSocketExtension
+	public static bool IsConnectionEnable(this WebSocket socket)
 	{
-		public static bool IsConnectionEnable(this WebSocket socket)
+		switch (socket.State)
 		{
-			switch (socket.State)
-			{
-				case WebSocketState.None:
-				case WebSocketState.CloseSent:
-				case WebSocketState.CloseReceived:
-				case WebSocketState.Closed:
-				case WebSocketState.Aborted:
-					{ }
-					break;
-				case WebSocketState.Connecting:
-				case WebSocketState.Open:
-					{
-						return true;
-					}
-			}
-			return false;
-		}
-
-		public static async Task<WebSocketReceiveDataResult> ReceiveByteArrayAsync(
-			this WebSocket webSocket,
-			BytesBuffer? receiveBuffer,
-			CancellationToken cancellationToken,
-			WebSocketReceiveDataResult? receiveDataResultSpecified = null)
-		{
-			if (receiveBuffer == null)
-			{
-				receiveBuffer = new BytesBuffer();
-			}
-			else
-			{
-				receiveBuffer.Clear();
-			}
-
-			WebSocketReceiveResult receiveResult;
-			do
-			{
-				var requestBodyBufferBytes
-					= receiveBuffer.GetEmptyBufferSegment();
-				receiveResult
-					= await webSocket.ReceiveAsync(
-						requestBodyBufferBytes,
-						cancellationToken);
-				// !!!
-				receiveBuffer.BytesCount += receiveResult.Count;
-				// !!!
-			} while (receiveResult.EndOfMessage == false);
-
-			receiveDataResultSpecified ??= new WebSocketReceiveDataResult();
-			////////////////////////////////////////////////
-			// !!! 
-			receiveDataResultSpecified.Result = receiveResult;
-			receiveDataResultSpecified.BytesReceived = receiveBuffer.ToBytes();
-			// !!!
-			////////////////////////////////////////////////
-			return receiveDataResultSpecified;
-		}
-
-		public static async Task<WebSocketReceiveDataResult> ReceiveStringAsync(
-			this WebSocket webSocket,
-			BytesBuffer? receiveBuffer,
-			CancellationToken cancellationToken,
-			System.Text.Encoding? textEncoding = null,
-			WebSocketReceiveDataResult? receiveDataResultSpecified = null)
-		{
-			var receiveResult = await WebSocketExtension.ReceiveByteArrayAsync(
-				webSocket,
-				receiveBuffer,
-				cancellationToken,
-				receiveDataResultSpecified);
-			var bytesReceived = receiveResult.BytesReceived;
-			if (bytesReceived?.Length > 0)
-			{
-				textEncoding ??= System.Text.Encoding.UTF8;
-				// !!!
-				receiveResult.StringReceived = textEncoding.GetString(bytesReceived);
-				// !!!
-			}
-			return receiveResult;
-		}
-
-		public static async Task<WebSocketReceiveDataResult> ReceiveJsonDocumentAsync(
-			this WebSocket webSocket,
-			BytesBuffer? receiveBuffer,
-			CancellationToken cancellationToken,
-			System.Text.Encoding? textEncoding = null,
-			JsonDocumentOptions jsonDeserializeOptions = default,
-			WebSocketReceiveDataResult? receiveDataResultSpecified = null)
-		{
-			var receiveResult = await WebSocketExtension.ReceiveStringAsync(
-				webSocket,
-				receiveBuffer,
-				cancellationToken,
-				textEncoding,
-				receiveDataResultSpecified);
-			var stringReceived = receiveResult.StringReceived;
-			if (stringReceived?.Length > 0)
-			{
-				var jsonDocumentReceived = JsonDocument.Parse(stringReceived, jsonDeserializeOptions);
+			case WebSocketState.None:
+			case WebSocketState.CloseSent:
+			case WebSocketState.CloseReceived:
+			case WebSocketState.Closed:
+			case WebSocketState.Aborted:
+				{ }
+				break;
+			case WebSocketState.Connecting:
+			case WebSocketState.Open:
 				{
-					// !!!
-					receiveResult.JsonDocumentReceived = jsonDocumentReceived;
-					// !!!
+					return true;
 				}
-			}
-			return receiveResult;
+		}
+		return false;
+	}
+
+	public static async Task<WebSocketReceiveDataResult> ReceiveByteArrayAsync(
+		this WebSocket webSocket,
+		BytesBuffer? receiveBuffer,
+		CancellationToken cancellationToken,
+		WebSocketReceiveDataResult? receiveDataResultSpecified = null)
+	{
+		if (receiveBuffer == null)
+		{
+			receiveBuffer = new BytesBuffer();
+		}
+		else
+		{
+			receiveBuffer.Clear();
 		}
 
-		public static async Task<WebSocketReceiveObjectResult<ObjectType>> ReceiveObjectAsync<ObjectType>(
-			this WebSocket webSocket,
-			BytesBuffer? receiveBuffer,
-			CancellationToken cancellationToken,
-			System.Text.Encoding? textEncoding = null)
+		WebSocketReceiveResult receiveResult;
+		do
 		{
-			var receiveObjectResult = new WebSocketReceiveObjectResult<ObjectType>();
-			var receiveResult = await WebSocketExtension.ReceiveStringAsync(
-				webSocket,
-				receiveBuffer,
-				cancellationToken,
-				textEncoding,
-				receiveObjectResult);
-			if (receiveResult != receiveObjectResult)
-			{
-				throw new ApplicationException("“receiveResult”应当为指定的“receiveObjectResult”对象。");
-			}
-			var stringReceived = receiveResult.StringReceived;
-			if (stringReceived?.Length > 0)
+			var requestBodyBufferBytes
+				= receiveBuffer.GetEmptyBufferSegment();
+			receiveResult
+				= await webSocket.ReceiveAsync(
+					requestBodyBufferBytes,
+					cancellationToken);
+			// !!!
+			receiveBuffer.BytesCount += receiveResult.Count;
+			// !!!
+		} while (receiveResult.EndOfMessage == false);
+
+		receiveDataResultSpecified ??= new WebSocketReceiveDataResult();
+		////////////////////////////////////////////////
+		// !!! 
+		receiveDataResultSpecified.Result = receiveResult;
+		receiveDataResultSpecified.BytesReceived = receiveBuffer.ToBytes();
+		// !!!
+		////////////////////////////////////////////////
+		return receiveDataResultSpecified;
+	}
+
+	public static async Task<WebSocketReceiveDataResult> ReceiveStringAsync(
+		this WebSocket webSocket,
+		BytesBuffer? receiveBuffer,
+		CancellationToken cancellationToken,
+		System.Text.Encoding? textEncoding = null,
+		WebSocketReceiveDataResult? receiveDataResultSpecified = null)
+	{
+		var receiveResult = await WebSocketExtension.ReceiveByteArrayAsync(
+			webSocket,
+			receiveBuffer,
+			cancellationToken,
+			receiveDataResultSpecified);
+		var bytesReceived = receiveResult.BytesReceived;
+		if (bytesReceived?.Length > 0)
+		{
+			textEncoding ??= System.Text.Encoding.UTF8;
+			// !!!
+			receiveResult.StringReceived = textEncoding.GetString(bytesReceived);
+			// !!!
+		}
+		return receiveResult;
+	}
+
+	public static async Task<WebSocketReceiveDataResult> ReceiveJsonDocumentAsync(
+		this WebSocket webSocket,
+		BytesBuffer? receiveBuffer,
+		CancellationToken cancellationToken,
+		System.Text.Encoding? textEncoding = null,
+		JsonDocumentOptions jsonDeserializeOptions = default,
+		WebSocketReceiveDataResult? receiveDataResultSpecified = null)
+	{
+		var receiveResult = await WebSocketExtension.ReceiveStringAsync(
+			webSocket,
+			receiveBuffer,
+			cancellationToken,
+			textEncoding,
+			receiveDataResultSpecified);
+		var stringReceived = receiveResult.StringReceived;
+		if (stringReceived?.Length > 0)
+		{
+			var jsonDocumentReceived = JsonDocument.Parse(stringReceived, jsonDeserializeOptions);
 			{
 				// !!!
-				receiveObjectResult.ObjectReceived = stringReceived.ToObjectByJsonDeserialize<ObjectType>();
+				receiveResult.JsonDocumentReceived = jsonDocumentReceived;
 				// !!!
 			}
-			return receiveObjectResult;
+		}
+		return receiveResult;
+	}
+
+	public static async Task<WebSocketReceiveObjectResult<ObjectType>> ReceiveObjectAsync<ObjectType>(
+		this WebSocket webSocket,
+		BytesBuffer? receiveBuffer,
+		CancellationToken cancellationToken,
+		System.Text.Encoding? textEncoding = null)
+	{
+		var receiveObjectResult = new WebSocketReceiveObjectResult<ObjectType>();
+		var receiveResult = await WebSocketExtension.ReceiveStringAsync(
+			webSocket,
+			receiveBuffer,
+			cancellationToken,
+			textEncoding,
+			receiveObjectResult);
+		if (receiveResult != receiveObjectResult)
+		{
+			throw new ApplicationException("“receiveResult”应当为指定的“receiveObjectResult”对象。");
+		}
+		var stringReceived = receiveResult.StringReceived;
+		if (stringReceived?.Length > 0)
+		{
+			// !!!
+			receiveObjectResult.ObjectReceived = stringReceived.ToObjectByJsonDeserialize<ObjectType>();
+			// !!!
+		}
+		return receiveObjectResult;
+	}
+
+	public static async IAsyncEnumerable<WebSocketReceiveDataResult?> ReceiveByteArraysAsync(
+		this WebSocket webSocket,
+		BytesBuffer? receiveBuffer,
+		[EnumeratorCancellation] CancellationToken cancellationToken)
+	{
+		if (receiveBuffer == null)
+		{
+			yield break;
 		}
 
-		public static async IAsyncEnumerable<WebSocketReceiveDataResult?> ReceiveByteArraysAsync(
-			this WebSocket webSocket,
-			BytesBuffer? receiveBuffer,
-			[EnumeratorCancellation] CancellationToken cancellationToken)
+		WebSocketReceiveDataResult? receiveDataResult = null;
+		while (receiveDataResult?.Result?.CloseStatus == null)
 		{
-			if (receiveBuffer == null)
-			{
-				yield break;
-			}
+			receiveDataResult
+				= await WebSocketExtension.ReceiveByteArrayAsync(
+					webSocket,
+					receiveBuffer,
+					cancellationToken);
+			// !!!
+			yield return receiveDataResult;
+			// !!!
+		}
+	}
 
-			WebSocketReceiveDataResult? receiveDataResult = null;
-			while (receiveDataResult?.Result?.CloseStatus == null)
-			{
-				receiveDataResult
-					= await WebSocketExtension.ReceiveByteArrayAsync(
-						webSocket,
-						receiveBuffer,
+	public static async IAsyncEnumerable<WebSocketReceiveDataResult?> ReceiveStringsAsync(
+		this WebSocket webSocket,
+		BytesBuffer? receiveBuffer,
+		[EnumeratorCancellation] CancellationToken cancellationToken,
+		System.Text.Encoding? textEncoding = null)
+	{
+		if (receiveBuffer == null)
+		{
+			yield break;
+		}
+
+		WebSocketReceiveDataResult? receiveDataResult = null;
+		while (receiveDataResult?.Result?.CloseStatus == null)
+		{
+			receiveDataResult
+				= await WebSocketExtension.ReceiveStringAsync(
+					webSocket,
+					receiveBuffer,
+					cancellationToken,
+					textEncoding);
+			// !!!
+			yield return receiveDataResult;
+			// !!!
+		}
+	}
+
+	public static async IAsyncEnumerable<WebSocketReceiveDataResult?> ReceiveJsonDocumentsAsync(
+		this WebSocket webSocket,
+		BytesBuffer? receiveBuffer,
+		[EnumeratorCancellation] CancellationToken cancellationToken,
+		System.Text.Encoding? textEncoding = null,
+		JsonDocumentOptions jsonDeserializeOptions = default)
+	{
+		if (receiveBuffer == null)
+		{
+			yield break;
+		}
+
+		WebSocketReceiveDataResult? receiveDataResult = null;
+		while (receiveDataResult?.Result?.CloseStatus == null)
+		{
+			receiveDataResult
+				= await WebSocketExtension.ReceiveJsonDocumentAsync(
+					webSocket,
+					receiveBuffer,
+					cancellationToken,
+					textEncoding,
+					jsonDeserializeOptions);
+			// !!!
+			yield return receiveDataResult;
+			// !!!
+		}
+	}
+
+	public static async IAsyncEnumerable<WebSocketReceiveObjectResult<ObjectType>?> ReceiveObjectsAsync<ObjectType>(
+		this WebSocket webSocket,
+		BytesBuffer? receiveBuffer,
+		[EnumeratorCancellation] CancellationToken cancellationToken,
+		System.Text.Encoding? textEncoding = null)
+	{
+		if (receiveBuffer == null)
+		{
+			yield break;
+		}
+
+		WebSocketReceiveObjectResult<ObjectType>? receiveObjectResult = null;
+		while (receiveObjectResult?.Result?.CloseStatus == null)
+		{
+			receiveObjectResult
+				= await WebSocketExtension.ReceiveObjectAsync<ObjectType>(
+					webSocket,
+					receiveBuffer,
+					cancellationToken,
+					textEncoding);
+			// !!!
+			yield return receiveObjectResult;
+			// !!!
+		}
+	}
+
+
+	public static async Task<bool> TryToCloseAsync(
+		this WebSocket webSocket,
+		WebSocketCloseStatus closeStatus,
+		string? statusDescription,
+		CancellationToken cancellationToken)
+	{
+		switch (webSocket.State)
+		{
+			case WebSocketState.None:
+			case WebSocketState.Closed:
+			case WebSocketState.Aborted:
+			case WebSocketState.CloseSent:
+				{
+					return false;
+				}
+			case WebSocketState.CloseReceived:
+				{
+					await webSocket.CloseOutputAsync(
+						closeStatus,
+						statusDescription,
 						cancellationToken);
-				// !!!
-				yield return receiveDataResult;
-				// !!!
-			}
+				}
+				break;
+			default:
+			case WebSocketState.Connecting:
+			case WebSocketState.Open:
+				{
+					await webSocket.CloseAsync(
+						closeStatus,
+						statusDescription,
+						cancellationToken);
+				}
+				break;
 		}
-
-		public static async IAsyncEnumerable<WebSocketReceiveDataResult?> ReceiveStringsAsync(
-			this WebSocket webSocket,
-			BytesBuffer? receiveBuffer,
-			[EnumeratorCancellation] CancellationToken cancellationToken,
-			System.Text.Encoding? textEncoding = null)
-		{
-			if (receiveBuffer == null)
-			{
-				yield break;
-			}
-
-			WebSocketReceiveDataResult? receiveDataResult = null;
-			while (receiveDataResult?.Result?.CloseStatus == null)
-			{
-				receiveDataResult
-					= await WebSocketExtension.ReceiveStringAsync(
-						webSocket,
-						receiveBuffer,
-						cancellationToken,
-						textEncoding);
-				// !!!
-				yield return receiveDataResult;
-				// !!!
-			}
-		}
-
-		public static async IAsyncEnumerable<WebSocketReceiveDataResult?> ReceiveJsonDocumentsAsync(
-			this WebSocket webSocket,
-			BytesBuffer? receiveBuffer,
-			[EnumeratorCancellation] CancellationToken cancellationToken,
-			System.Text.Encoding? textEncoding = null,
-			JsonDocumentOptions jsonDeserializeOptions = default)
-		{
-			if (receiveBuffer == null)
-			{
-				yield break;
-			}
-
-			WebSocketReceiveDataResult? receiveDataResult = null;
-			while (receiveDataResult?.Result?.CloseStatus == null)
-			{
-				receiveDataResult
-					= await WebSocketExtension.ReceiveJsonDocumentAsync(
-						webSocket,
-						receiveBuffer,
-						cancellationToken,
-						textEncoding,
-						jsonDeserializeOptions);
-				// !!!
-				yield return receiveDataResult;
-				// !!!
-			}
-		}
-
-		public static async IAsyncEnumerable<WebSocketReceiveObjectResult<ObjectType>?> ReceiveObjectsAsync<ObjectType>(
-			this WebSocket webSocket,
-			BytesBuffer? receiveBuffer,
-			[EnumeratorCancellation] CancellationToken cancellationToken,
-			System.Text.Encoding? textEncoding = null)
-		{
-			if (receiveBuffer == null)
-			{
-				yield break;
-			}
-
-			WebSocketReceiveObjectResult<ObjectType>? receiveObjectResult = null;
-			while (receiveObjectResult?.Result?.CloseStatus == null)
-			{
-				receiveObjectResult
-					= await WebSocketExtension.ReceiveObjectAsync<ObjectType>(
-						webSocket,
-						receiveBuffer,
-						cancellationToken,
-						textEncoding);
-				// !!!
-				yield return receiveObjectResult;
-				// !!!
-			}
-		}
-
-
-		public static async Task<bool> TryToCloseAsync(
-			this WebSocket webSocket,
-			WebSocketCloseStatus closeStatus,
-			string? statusDescription,
-			CancellationToken cancellationToken)
-		{
-			switch (webSocket.State)
-			{
-				case WebSocketState.None:
-				case WebSocketState.Closed:
-				case WebSocketState.Aborted:
-				case WebSocketState.CloseSent:
-					{
-						return false;
-					}
-				case WebSocketState.CloseReceived:
-					{
-						await webSocket.CloseOutputAsync(
-							closeStatus,
-							statusDescription,
-							cancellationToken);
-					}
-					break;
-				default:
-				case WebSocketState.Connecting:
-				case WebSocketState.Open:
-					{
-						await webSocket.CloseAsync(
-							closeStatus,
-							statusDescription,
-							cancellationToken);
-					}
-					break;
-			}
-			return true;
-		}
+		return true;
 	}
 }

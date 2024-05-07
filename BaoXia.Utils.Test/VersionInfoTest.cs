55b8643404b4b6b2df9ea34ed493576c5e265c42
ZhuXiaoChun <zhu.xiaochun@qq.com>
2024-05-07 18:24:35 +0800

    增加了：“VersionInfo”对象。

---
+++ BaoXia.Utils.Test/VersionInfoTest.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaoXia.Utils.Test;
+
+[TestClass]
+public class VersionInfoTest
+{
+
+	////////////////////////////////////////////////
+	// @自身实现
+	////////////////////////////////////////////////
+
+	#region 自身实现
+
+	[TestMethod]
+	public void CompareToTest()
+	{
+		var version_1_0_0 = new VersionInfo { VersionString = "1.0.0" };
+		var version_1_0_0b = new VersionInfo { VersionString = "1.0.0" };
+		var version_1_0_1 = new VersionInfo { VersionString = "1.0.1" };
+		var version_1_1_0 = new VersionInfo { VersionString = "1.1.0" };
+		var version_1_1_1 = new VersionInfo { VersionString = "1.1.1" };
+		var version_2_1_1 = new VersionInfo { VersionString = "2.1.1" };
+		var version_1_0 = new VersionInfo { VersionString = "1.0" };
+		var version_1_0_0_0 = new VersionInfo { VersionString = "1.0.0.0" };
+
+#pragma warning disable CS1718 // 对同一变量进行了比较
+		Assert.IsTrue(version_1_0_0 == version_1_0_0);
+#pragma warning restore CS1718 // 对同一变量进行了比较
+
+		Assert.IsTrue(version_1_0_0 == version_1_0_0b);
+		Assert.IsTrue(version_1_0_0 < version_1_0_1);
+		Assert.IsTrue(version_1_0_0 < version_1_1_0);
+		Assert.IsTrue(version_1_0_0 < version_1_1_1);
+		Assert.IsTrue(version_1_0_0 < version_2_1_1);
+		Assert.IsTrue(version_1_0_0 > version_1_0);
+		Assert.IsTrue(version_1_0_0 < version_1_0_0_0);
+
+		Assert.IsTrue(version_1_1_1 > version_1_0_0b);
+		Assert.IsTrue(version_1_1_1 > version_1_0_1);
+		Assert.IsTrue(version_1_1_1 > version_1_1_0);
+#pragma warning disable CS1718 // 对同一变量进行了比较
+		Assert.IsTrue(version_1_1_1 == version_1_1_1);
+#pragma warning restore CS1718 // 对同一变量进行了比较
+		Assert.IsTrue(version_1_1_1 < version_2_1_1);
+		Assert.IsTrue(version_1_1_1 > version_1_0);
+		Assert.IsTrue(version_1_1_1 < version_1_0_0_0);
+	}
+
+	#endregion
+}
---
+++ BaoXia.Utils/VersionInfo.cs
@@ -0,0 +1,208 @@
+﻿using BaoXia.Utils.Extensions;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaoXia.Utils;
+
+public class VersionInfo
+{
+	////////////////////////////////////////////////
+	// @静态常量
+	////////////////////////////////////////////////
+
+	#region 静态常量
+
+	public const char VersionSectionSplitChar = '.';
+
+	#endregion
+
+
+	////////////////////////////////////////////////
+	// @自身属性
+	////////////////////////////////////////////////
+
+	#region 自身属性
+
+	protected string _versionString = string.Empty;
+
+	public string VersionString
+	{
+		get
+		{
+
+			return _versionString;
+		}
+		set
+		{
+			if (StringUtil.EqualsStrings(_versionString, value))
+			{
+				return;
+			}
+
+			_versionString = value;
+			VersionSectionNumbers = DidCreateVersionSectionNumbersFromString(_versionString);
+		}
+	} 
+
+	public int[] VersionSectionNumbers { get; private set; } = [];
+
+	public int VersionSectionsCount => VersionSectionNumbers.Length;
+
+	#endregion
+
+
+	////////////////////////////////////////////////
+	// @类方法
+	////////////////////////////////////////////////
+
+	#region 类方法
+
+	public static bool operator ==(VersionInfo? versionInfoA, VersionInfo? versionInfoB)
+	{
+		var isVersionInfoA_Null = versionInfoA is null;
+		var isVersionInfoB_Null = versionInfoB is null;
+		if (isVersionInfoA_Null
+			&& isVersionInfoB_Null)
+		{
+			return true;
+		}
+		else if (isVersionInfoA_Null
+			|| isVersionInfoB_Null)
+		{
+			return false;
+		}
+		return versionInfoA!.CompareTo(versionInfoB) == 0;
+	}
+
+	public static bool operator !=(VersionInfo versionInfoA, VersionInfo versionInfoB)
+	{
+		return versionInfoA.CompareTo(versionInfoB) != 0;
+	}
+
+	public static bool operator >(VersionInfo versionInfoA, VersionInfo versionInfoB)
+	{
+		return versionInfoA.CompareTo(versionInfoB) > 0;
+	}
+
+
+	public static bool operator <(VersionInfo versionInfoA, VersionInfo versionInfoB)
+	{
+		return versionInfoA.CompareTo(versionInfoB) < 0;
+	}
+
+	#endregion
+
+
+	////////////////////////////////////////////////
+	// @自身实现
+	////////////////////////////////////////////////
+
+	#region 自身实现
+
+	public VersionInfo()
+	{ }
+
+	public VersionInfo(string versionString)
+	{
+		VersionString = versionString;
+	}
+
+
+	public int CompareTo(VersionInfo? anotherVersionInfo)
+	{
+		if (anotherVersionInfo == null)
+		{
+			return 1;
+		}
+
+		var versionSectionsCount = VersionSectionNumbers.Length;
+		var anotherVersionSectionsCount = anotherVersionInfo.VersionSectionNumbers.Length;
+		if (versionSectionsCount > anotherVersionSectionsCount)
+		{
+			return 1;
+		}
+		else if (versionSectionsCount < anotherVersionSectionsCount)
+		{
+			return -1;
+		}
+
+		for (var versionSectionIndex = 0;
+		    versionSectionIndex < versionSectionsCount;
+		    versionSectionIndex++)
+		{
+			var versionSectionNumber = VersionSectionNumbers[versionSectionIndex];
+			var anotherVersionSectionNumber = anotherVersionInfo.VersionSectionNumbers[versionSectionIndex];
+			if (versionSectionNumber > anotherVersionSectionNumber)
+			{
+				return 1;
+			}
+			if (versionSectionNumber < anotherVersionSectionNumber)
+			{
+				return -1;
+			}
+		}
+		return 0;
+	}
+
+	#endregion
+
+
+	////////////////////////////////////////////////
+	// @事件节点
+	////////////////////////////////////////////////
+
+	#region 事件节点
+
+	protected virtual int[] DidCreateVersionSectionNumbersFromString(string? versionString)
+	{
+		if (string.IsNullOrEmpty(versionString))
+		{
+			return [];
+		}
+
+		var versionSectionStrings = versionString.Split(VersionSectionSplitChar);
+		if (versionSectionStrings.Length < 1)
+		{
+			return [];
+		}
+
+		var versionSectionNumbers = new int[versionSectionStrings.Length];
+		for (var i = 0; i < versionSectionStrings.Length; i++)
+		{
+			_ = int.TryParse(versionSectionStrings[i], out var versionSectionNumber);
+			// !!!
+			versionSectionNumbers[i] = versionSectionNumber;
+			// !!!
+		}
+		return versionSectionNumbers;
+	}
+
+	#endregion
+
+
+	////////////////////////////////////////////////
+	// @重载“Object”
+	////////////////////////////////////////////////
+
+	#region 重载“Object”
+
+	public override bool Equals(object? obj)
+	{
+		if (obj is not VersionInfo anotherVersionInfo)
+		{
+			return false;
+		}
+		return CompareTo(anotherVersionInfo) == 0;
+	}
+
+	public override int GetHashCode()
+	{
+		return _versionString?.GetHashCode() ?? 0;
+	}
+
+	#endregion
+}

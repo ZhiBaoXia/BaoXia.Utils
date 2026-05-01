7e64d7c625a6bc5bb6f6c95b9b29951190addbf1
ZhuXiaoChun <zhu.xiaochun@qq.com>
2026-05-01 20:28:15 +0800

    feat: 添加线程池信息模型和工具类
    添加 ThreadPoolInfo 模型用于存储线程池状态信息，并实现 ThreadPoolUtil 工具类提供获取线程池信息的方法

---
+++ BaoXia.Utils/Models/ThreadPoolInfo.cs
@@ -0,0 +1,42 @@
+﻿namespace BaoXia.Utils.Models;
+
+public class ThreadPoolInfo
+{
+	////////////////////////////////////////////////
+	// @自身属性
+	////////////////////////////////////////////////
+
+	#region 自身属性
+
+	public int SystemThreadsCountMax
+	{
+		get
+		{
+			return this.SystemWorkerThreadsCountMax
+			    + this.SystemIOThreadsCountMax;
+		}
+	}
+
+	public int SystemThreadsCount
+	{
+		get
+		{
+			return this.SystemWorkerThreadsCount
+			    + this.SystemIOThreadsCount;
+		}
+	}
+
+	public int SystemWorkerThreadsCountMax { get; set; }
+
+	public int SystemWorkerThreadsCount { get; set; }
+
+	public int SystemIOThreadsCountMax { get; set; }
+
+	public int SystemIOThreadsCount { get; set; }
+
+	public int SystemThreadTasksCountWaitingToWork { get; set; }
+
+	public int SystemThreadsCountInPool { get; set; }
+
+	#endregion
+}
---
+++ BaoXia.Utils/ThreadPoolUtil.cs
@@ -0,0 +1,34 @@
+﻿using BaoXia.Utils.Models;
+using System.Threading;
+
+namespace BaoXia.Utils;
+
+public class ThreadPoolUtil
+{
+
+	////////////////////////////////////////////////
+	// @类方法
+	////////////////////////////////////////////////
+
+	#region 类方法
+
+	public static ThreadPoolInfo GetThreadPoolInfo()
+	{
+		var threadPoolInfo = new ThreadPoolInfo();
+
+		ThreadPool.GetMaxThreads(out var workerThreadsCountMax, out var completionPortThreadsCountMax);
+		threadPoolInfo.SystemWorkerThreadsCountMax = workerThreadsCountMax;
+		threadPoolInfo.SystemIOThreadsCountMax = completionPortThreadsCountMax;
+
+		ThreadPool.GetAvailableThreads(out var workerThreadsCount, out var completionPortThreadsCount);
+		threadPoolInfo.SystemWorkerThreadsCount = workerThreadsCountMax - workerThreadsCount;
+		threadPoolInfo.SystemIOThreadsCount = completionPortThreadsCountMax - completionPortThreadsCount;
+
+		threadPoolInfo.SystemThreadTasksCountWaitingToWork = (int)ThreadPool.PendingWorkItemCount;
+		threadPoolInfo.SystemThreadsCountInPool = ThreadPool.ThreadCount;
+
+		return threadPoolInfo;
+	}
+
+	#endregion
+}

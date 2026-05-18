69381c70721f914df6574ac532ccc3907971fe80
ZhuXiaoChun <zhu.xiaochun@qq.com>
2026-05-18 11:10:45 +0800

    feat(Utils): 添加CompareUtil类用于多条件比较
    新增CompareUtil工具类，提供通用的多条件比较结果聚合方法，支持传入多个比较逻辑依次判断并返回首个非0结果

---
+++ BaoXia.Utils/CompareUtil.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BaoXia.Utils;
+
+public class CompareUtil
+{
+	////////////////////////////////////////////////
+	// @类方法
+	////////////////////////////////////////////////
+
+	#region 类方法
+
+	public static int GetCompareResultOf<ItemType>(
+		ItemType itemA, ItemType itemB, params Func<ItemType, ItemType, int>[] toCompareResults)
+	{
+		foreach (var toCompareResult in toCompareResults)
+		{
+			var compareResult = toCompareResult(itemA, itemB);
+			if (compareResult != 0)
+			{
+				return compareResult;
+			}
+		}
+		return 0;
+	}
+
+	#endregion
+}

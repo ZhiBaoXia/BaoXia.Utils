4cacb16139d7f3167e943a6275bf82a119b8b298
ZhuXiaoChun <zhu.xiaochun@qq.com>
2026-06-24 23:36:03 +0800

    refactor(TimeSpanExtension): 统一变量命名并新增剩余时间格式化方法
    1. 将TitleOfDetailPageDefault方法内的caption变量重命名为title，保持命名一致性
    2. 新增TitleOfRemainingTime扩展方法，用于格式化剩余时间展示

--- BaoXia.Utils/Extensions/TimeSpanExtension.cs
+++ BaoXia.Utils/Extensions/TimeSpanExtension.cs
@@ -77,66 +77,88 @@ public static class TimeSpanExtension
 
 	public static string TitleOfDetailPageDefault(this TimeSpan timeSpan)
 	{
-		string caption;
+		string title;
 		if (timeSpan.TotalMinutes < 1)
 		{
-			caption = $"{timeSpan.TotalSeconds:F0}秒";
+			title = $"{timeSpan.TotalSeconds:F0}秒";
 		}
 		else if (timeSpan.TotalHours < 1)
 		{
 			if (timeSpan.Seconds > 0)
 			{
-				caption = $"{timeSpan.Minutes:0}分钟, {timeSpan.Seconds:0}秒";
+				title = $"{timeSpan.Minutes:0}分钟, {timeSpan.Seconds:0}秒";
 			}
 			else
 			{
-				caption = $"{timeSpan.Minutes:0}分钟";
+				title = $"{timeSpan.Minutes:0}分钟";
 			}
 		}
 		else if (timeSpan.TotalDays < 1)
 		{
 			if (timeSpan.Seconds > 0)
 			{
-				caption = $"{timeSpan.Hours:0}小时, {timeSpan.Minutes:0}分钟, {timeSpan.Seconds:0}秒";
+				title = $"{timeSpan.Hours:0}小时, {timeSpan.Minutes:0}分钟, {timeSpan.Seconds:0}秒";
 			}
 			else if (timeSpan.Minutes > 0)
 			{
-				caption = $"{timeSpan.Hours:0}小时, {timeSpan.Minutes:0}分钟";
+				title = $"{timeSpan.Hours:0}小时, {timeSpan.Minutes:0}分钟";
 			}
 			else
 			{
-				caption = $"{timeSpan.Hours:0}小时";
+				title = $"{timeSpan.Hours:0}小时";
 			}
 		}
 		else if (timeSpan.TotalDays < TimeDefinition.Forever.TotalDays)
 		{
 			if (timeSpan.Seconds > 0)
 			{
-				caption = $"{timeSpan.Days:0}天, {timeSpan.Hours:0}小时, {timeSpan.Minutes:0}分钟, {timeSpan.Seconds:0}秒";
+				title = $"{timeSpan.Days:0}天, {timeSpan.Hours:0}小时, {timeSpan.Minutes:0}分钟, {timeSpan.Seconds:0}秒";
 			}
 			else if (timeSpan.Minutes > 0)
 			{
-				caption = $"{timeSpan.Days:0}天, {timeSpan.Hours:0}小时, {timeSpan.Minutes:0}分钟";
+				title = $"{timeSpan.Days:0}天, {timeSpan.Hours:0}小时, {timeSpan.Minutes:0}分钟";
 			}
 			else if (timeSpan.Hours > 0)
 			{
-				caption = $"{timeSpan.Days:0}天, {timeSpan.Hours:0}小时";
+				title = $"{timeSpan.Days:0}天, {timeSpan.Hours:0}小时";
 			}
 			else
 			{
-				caption = $"{timeSpan.Days:0}天";
+				title = $"{timeSpan.Days:0}天";
 			}
 		}
 		else
 		{
-			caption = "永久";
+			title = "永久";
 		}
-		return caption;
+		return title;
 	}
 
 	public static string TitleOfDetailPageDefault(this double totalSeconds)
 	{
 		return TimeSpanExtension.TitleOfDetailPageDefault(TimeSpanUtil.FromSeconds(totalSeconds));
+	}
+
+	public static string TitleOfRemainingTime(this TimeSpan remainingTime)
+	{
+		string remainingTimeTitle;
+		if (remainingTime.TotalMinutes < 1)
+		{
+			remainingTimeTitle = $"{remainingTime.TotalMinutes:0}秒";
+		}
+		else if (remainingTime.TotalHours < 1)
+		{
+			remainingTimeTitle = $"{remainingTime.TotalMinutes:0}分钟";
+		}
+		else if (remainingTime.TotalDays < 1)
+		{
+			remainingTimeTitle = $"{remainingTime.TotalHours:0}小时";
+		}
+		else
+		{
+			remainingTimeTitle = $"{remainingTime.TotalHours:1}天";
+		}
+		return remainingTimeTitle;
 	}
 
 	#endregion

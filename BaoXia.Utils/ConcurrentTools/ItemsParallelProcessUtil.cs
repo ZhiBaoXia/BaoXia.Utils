b14726e3b3eadebe0241fc6edb23b310983bdbe2
ZhuXiaoChun <zhu.xiaochun@qq.com>
2026-07-13 23:39:00 +0800

    feat(ConcurrentTools): 新增批量项并行处理工具类
    新增ItemsParallelProcessUtil工具类，提供线程安全的批量数据并行遍历处理能力，支持配置最大并行任务数

---
+++ BaoXia.Utils/ConcurrentTools/ItemsParallelProcessUtil.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BaoXia.Utils.ConcurrentTools;
+
+public class ItemsParallelProcessUtil
+{
+
+	////////////////////////////////////////////////
+	// @类方法
+	////////////////////////////////////////////////
+
+	#region 类方法
+
+	public static void ForEach<ItemType>(IEnumerable<ItemType> items, int tasksCountToProcessItemsMax, Action<ItemType> toProcessItem)
+	{
+		if (tasksCountToProcessItemsMax <= 1)
+		{
+			foreach (var item in items)
+			{
+				toProcessItem(item);
+			}
+			return;
+		}
+
+		Parallel.ForEach(items, new ParallelOptions
+		{
+			MaxDegreeOfParallelism = tasksCountToProcessItemsMax
+		}, toProcessItem);
+	}
+
+	#endregion
+
+
+}

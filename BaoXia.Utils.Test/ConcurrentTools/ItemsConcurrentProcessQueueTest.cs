e1baee46deebc88333c76770e9b33b0ea1a42267
ZhuXiaoChun <zhu.xiaochun@qq.com>
2026-07-28 19:01:58 +0800

    重命名并迁移ItemsConcurrentProcessQueue测试代码
    将ItemsConcurrentProcessQueue2相关实现和测试代码分别迁移并重命名为ItemsConcurrentProcessQueue及其测试。删除旧文件，新增ItemsConcurrentProcessQueueTest.cs，适配测试对象并统一代码风格，测试逻辑保持一致，仅做命名和格式调整。

--- BaoXia.Utils.Test/ConcurrentTools/ItemsConcurrentProcessQueueTest.cs
+++ BaoXia.Utils.Test/ConcurrentTools/ItemsConcurrentProcessQueueTest.cs
@@ -8,21 +8,21 @@ using System.Threading.Tasks;
 namespace BaoXia.Utils.Test.ConcurrentTools;
 
 [TestClass]
-public class ItemsConcurrentProcessQueue2Test
+public class ItemsConcurrentProcessQueueTest
 {
 	[TestMethod]
 	public async Task ProcessItem_ShouldUseHandlerOfCurrentItem()
 	{
-		var processQueue = new ItemsConcurrentProcessQueue2<int>(1);
-		var processTypes = new ConcurrentDictionary<int, string>();
+		var processQueue = new ItemsConcurrentProcessQueue<int>(1);
 		using var isFirstItemProcessing = new ManualResetEventSlim();
+		var processTypes = new ConcurrentDictionary<int, string>();
 		using var canFirstItemProcessCompleted = new ManualResetEventSlim();
 
 		processQueue.ProcessItem(1, (item) =>
 		{
 			processTypes[item] = "First";
 			isFirstItemProcessing.Set();
-			canFirstItemProcessCompleted.Wait(TimeSpan.FromSeconds(5));
+			canFirstItemProcessCompleted.Wait(TimeSpan.FromSeconds(5), TestContext.CancellationTokenSource.Token);
 		});
 		try
 		{
@@ -37,7 +37,7 @@ public class ItemsConcurrentProcessQueue2Test
 			canFirstItemProcessCompleted.Set();
 		}
 
-		await processQueue.WhenAll().WaitAsync(TimeSpan.FromSeconds(5));
+		await processQueue.WhenAll().WaitAsync(TimeSpan.FromSeconds(5), TestContext.CancellationTokenSource.Token);
 
 		Assert.AreEqual("First", processTypes[1]);
 		Assert.AreEqual("Second", processTypes[2]);
@@ -48,7 +48,7 @@ public class ItemsConcurrentProcessQueue2Test
 	{
 		var concurrencyCountMax = 4;
 		var itemsCount = 200;
-		var processQueue = new ItemsConcurrentProcessQueue2<int>(concurrencyCountMax);
+		var processQueue = new ItemsConcurrentProcessQueue<int>(concurrencyCountMax);
 		var itemsCountProcessing = 0;
 		var itemsCountProcessingMax = 0;
 		var itemsCountProcessed = 0;
@@ -83,7 +83,7 @@ public class ItemsConcurrentProcessQueue2Test
 	[TestMethod]
 	public async Task ProcessItem_ShouldProcessAllItemsInRepeatedBatches()
 	{
-		var processQueue = new ItemsConcurrentProcessQueue2<int>(4);
+		var processQueue = new ItemsConcurrentProcessQueue<int>(4);
 		var batchesCount = 100;
 		var itemsCountInBatch = 100;
 		var itemsCountProcessed = 0;
@@ -125,4 +125,6 @@ public class ItemsConcurrentProcessQueue2Test
 			valueMaxCurrent = valueMaxPrev;
 		}
 	}
+
+	public TestContext TestContext { get; set; }
 }

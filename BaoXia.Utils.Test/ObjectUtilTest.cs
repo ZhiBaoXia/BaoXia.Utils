21163727243fc814c26b589f8a4acd2f48818106
ZhuXiaoChun <zhu.xiaochun@qq.com>
2026-08-05 18:04:23 +0800

    feat(ObjectUtil): 添加获取属性路径的工具方法
    新增GetPropertyPath泛型方法，支持通过表达式获取对象的属性访问路径，同时添加了对应的单元测试用例覆盖正常场景和非法表达式场景

---
+++ BaoXia.Utils.Test/ObjectUtilTest.cs
@@ -0,0 +1,36 @@
+﻿using BaoXia.Utils.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BaoXia.Utils.Test;
+
+[TestClass]
+public class ObjectUtilTest
+{
+	[TestMethod]
+	public void GetPropertyPathTest()
+	{
+		var nestedPropertyPath = ObjectUtil.GetPropertyPath<ObjectPropertyInfo>(
+			objectPropertyInfo => objectPropertyInfo.PropertyInfo.Name);
+		Assert.AreEqual(
+			$"{nameof(ObjectPropertyInfo.PropertyInfo)}.{nameof(System.Reflection.PropertyInfo.Name)}",
+			nestedPropertyPath);
+
+		var propertyPath = ObjectUtil.GetPropertyPath<ObjectPropertyInfo>(
+			objectPropertyInfo => objectPropertyInfo.Id);
+		Assert.AreEqual(
+			nameof(ObjectPropertyInfo.Id),
+			propertyPath);
+	}
+
+	[TestMethod]
+	public void GetPropertyPathWithInvalidExpressionTest()
+	{
+		Assert.ThrowsExactly<ArgumentException>(() =>
+			ObjectUtil.GetPropertyPath<ObjectPropertyInfo>(
+				objectPropertyInfo => DateTime.MinValue));
+		Assert.ThrowsExactly<ArgumentException>(() =>
+			ObjectUtil.GetPropertyPath<ObjectPropertyInfo>(
+				objectPropertyInfo => objectPropertyInfo.ToString()));
+	}
+}
--- BaoXia.Utils/ObjectUtil.cs
+++ BaoXia.Utils/ObjectUtil.cs
@@ -1,15 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
 
 namespace BaoXia.Utils;
 
 public static class ObjectUtil
 {
-
 	////////////////////////////////////////////////
 	// @类方法
 	////////////////////////////////////////////////
 
 	#region 类方法
+
+	public static string GetPropertyPath<ObjectType>(Expression<Func<ObjectType, object?>> propertyExpression)
+	{
+		//ArgumentNullException.ThrowIfNull(propertyExpression);
+
+		var propertyNames = new Stack<string>();
+		Expression? currentExpression = propertyExpression.Body;
+		if (currentExpression is UnaryExpression unaryExpression
+			&& (unaryExpression.NodeType == ExpressionType.Convert
+				|| unaryExpression.NodeType == ExpressionType.ConvertChecked))
+		{
+			currentExpression = unaryExpression.Operand;
+		}
+
+		while (currentExpression is MemberExpression memberExpression
+			&& memberExpression.Member is PropertyInfo propertyInfo)
+		{
+			propertyNames.Push(propertyInfo.Name);
+			currentExpression = memberExpression.Expression;
+		}
+
+		if (propertyNames.Count < 1
+			|| currentExpression != propertyExpression.Parameters[0])
+		{
+			throw new ArgumentException(
+				"表达式必须是从参数开始的属性访问路径。",
+				nameof(propertyExpression));
+		}
+
+		return string.Join('.', propertyNames);
+	}
 
 	public static object? CreateObject(Type objectType)
 	{

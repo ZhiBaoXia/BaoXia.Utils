f314b55980a29657e53949e78aa35f834b4bc877
ZhuXiaoChun <zhu.xiaochun@qq.com>
2026-08-27 19:09:39 +0800

    新增事件聚合数据重算及配置优化
    本次提交包括：
    - 新增 EventReducedDataController 及 ReducedData 接口，支持指定时间区间重算事件聚合数据
    - 新增 ReducedDataRequest 请求模型，包含统计起止时间
    - 在 EventReducedDataService 实现 ReducedDataInTimeRangeAsync，支持并发区间锁定
    - 新增 DateTimeRange 和 DateTimeOffsetRange 工具类
    - 日志类 Log 增加 Error 和 Warning 类型
    - 配置项 DefaultMaxSecondsOfEventRecordTriggerTimeBeforeNowToCache 从 1 小时调整为 30 天，并同步修改 ServiceConfig.json
    - 优化参数传递和代码格式，提升可读性与健壮性

---
+++ BaoXia.Utils/Models/DateTimeOffsetRange.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BaoXia.Utils.Models;
+
+public class DateTimeOffsetRange
+{
+	////////////////////////////////////////////////
+	// @自身属性
+	////////////////////////////////////////////////
+
+	#region 自身属性
+
+	public DateTimeOffset BeginTime { get; set; }
+
+	public DateTimeOffset EndTime { get; set; }
+
+	#endregion
+
+
+	////////////////////////////////////////////////
+	// @自身实现
+	////////////////////////////////////////////////
+
+	#region 自身实现
+
+	public DateTimeOffsetRange()
+	{ }
+
+	public DateTimeOffsetRange(DateTimeOffset beginTime, DateTimeOffset endTime)
+	{
+		BeginTime = beginTime;
+		EndTime = endTime;
+	}
+
+	#endregion
+}
---
+++ BaoXia.Utils/Models/DateTimeRange.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BaoXia.Utils.Models;
+
+public class DateTimeRange
+{
+	////////////////////////////////////////////////
+	// @自身属性
+	////////////////////////////////////////////////
+
+	#region 自身属性
+
+	public DateTime BeginTime { get; set; }
+
+	public DateTime EndTime { get; set; }
+
+	#endregion
+
+
+	////////////////////////////////////////////////
+	// @自身实现
+	////////////////////////////////////////////////
+
+	#region 自身实现
+
+	public DateTimeRange()
+	{ }
+
+	public DateTimeRange(DateTime beginTime, DateTime endTime)
+	{
+		BeginTime = beginTime;
+		EndTime = endTime;
+	}
+
+	#endregion
+}
